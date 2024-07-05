a96e8c08db9a03030c8da776082e7a9c1147141c
Hassan Ali <hassan.ali@my.utsa.edu>
2024-07-05 18:31:13 -0500

    removed strict mode

--- site/vite.config.js
+++ site/vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.com/HassanDev1/site",
+  base: "https://cdn.jsdelivr.net/gh/HassanDev1/site",
 });
