46b82ab7c3402875551c3885ac916ce16f5d5faf
Hassan Ali <hassan.ali@my.utsa.edu>
2024-07-06 18:19:51 -0500

    added pages

--- site/vite.config.js
+++ site/vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/site/",
   plugins: [react()],
-  base: "https://HassanDev1.github.io/site",
 });
